70e83dc30951a7bc41888276df1051d2fc352440
May Murillo <aam0714@cell02-cse.unt.ad.unt.edu>
2026-09-17 23:46:48 -0500

    Added reservation header file

---
+++ include/Reservation.h
@@ -0,0 +1,34 @@
+#ifndef Reservation.h
+#define Reservation.h
+#include <string>
+
+class Reservation {
+
+private:
+
+	int reservationID;
+	int studentID;
+	std::string studentName;
+	std::string resourceID;
+	std::string reservationDate;
+
+public:
+	Reservation();
+
+	void set_reservationID(int id);
+	int get_reservationID();
+
+	void set_studentID(int id);
+	int get_student();
+
+	void set_studentName(std::string name);
+	std::string get_studentName();
+
+	void set_resourceID(std::string id);
+	std::string get_resourceID();
+
+	void set_reservationDate(std::string date);
+	std::string getreservationDate();
+};
+
+#endif
