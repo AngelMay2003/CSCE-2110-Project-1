97ffea449259653fc7b95745ebe25229ff3e784a
david <muagba.david@gmail.com>
2026-09-20 19:28:18 -0500

    Fix manager declarations and add undo conflict check

--- include/ReservationManager.h
+++ include/ReservationManager.h
@@ -4,45 +4,67 @@
 #include "Reservation.h"
 #include "Resource.h"
 
+
+
 class ReservationManager {
-    private:
-    struct Node {
-        Reservation data;
-        Node* next;
+   private:
+   struct Node {
+       Reservation data;
+       Node* next;
 
-        Node(const Reservation& res) : data(res), next(nullptr) {}
-    };
 
-    Node* head;
 
-    struct WaitNode {
-        std::string studentId;
-        std::string resourceId;
-        WaitNode* next;
+       Node(const Reservation& res) : data(res), next(nullptr) {}
+   };
 
-        WaitNode(const std::string& sId, const std::string& rId) 
-        : studentsId(sId), resourceId(rId), next(nullptr) {}
-    };
 
-    WaitNode* waitFront;
-    WaitNode* waitRear;
-    int waitCount;
 
-    public:
-    ReservationManager();
-    ~ReservationManager();
+   Node* head;
 
-    void addActiveReservation(const Reservation& reservation);
-    bool removeActiveReservation(const std::string& reservationId);
-    bool findReservation(const std::string& reservationId) const;
-    void displayAllActiveReservation() const;
 
-    void addToWaitingList(const std::string& student, const std::string& resourceId);
-    bool removeFromWaitingList(const std::string& studentId, const std::string& resourceId);
-    void displayWaitingList() const;
-    bool WaitingListEmpty();
-    int waitingListSize();
+
+   struct WaitNode {
+       std::string studentId;
+       std::string resourceId;
+       WaitNode* next;
+
+
+
+       WaitNode(const std::string& sId, const std::string& rId)
+       : studentId(sId), resourceId(rId), next(nullptr) {}
+   };
+
+
+
+   WaitNode* waitFront;
+   WaitNode* waitRear;
+   int waitCount;
+
+
+
+   public:
+   ReservationManager();
+   ~ReservationManager();
+   ReservationManager(const ReservationManager&) = delete;
+   ReservationManager& operator=(const ReservationManager&) = delete;
+   bool canRestoreReservation(Reservation reservation) const;
+
+
+
+   void addActiveReservation(const Reservation& reservation);
+   bool removeActiveReservation(const std::string& reservationId);
+   bool findReservation(const std::string& reservationId) const;
+   void displayAllActiveReservation() const;
+
+
+
+   void addToWaitingList(const std::string& student, const std::string& resourceId);
+   bool removeFromWaitingList(const std::string& studentId, const std::string& resourceId);
+   void displayWaitingList() const;
+   bool isWaitingListEmpty();
+   int waitingListSize() const;
 };
 
-#endif
 
+
+#endif
